377fcf6a8ac5461c03a576f65d4ad6a1311fe855
Eduard.Zeleniuk <eduard.zeleniuk@gmail.com>
2018-01-25 15:50:53 +0200

    added contact-form-handler

---
+++ contact_form.php
@@ -0,0 +1,26 @@
+<!-- Contact Form -->
+									<section>
+										<form method="post" action="contact-form-handler.php">
+											<div class="row 50%">
+												<div class="6u 12u(mobile)">
+													<input type="text" name="name" value="Vasy Pupkin" id="contact-name" placeholder="Name" />
+												</div>
+												<div class="6u 12u(mobile)">
+													<input type="text" name="email" value="pupkin@mail.com" id="contact-email" placeholder="Email" />
+												</div>
+											</div>
+											<div class="row 50%">
+												<div class="12u">
+													<textarea name="message" id="contact-message" placeholder="Message" rows="4">Hello, I am Vasy Pupkin</textarea>
+												</div>
+											</div>
+											<div class="row">
+												<div class="12u">
+													<ul class="actions">
+														<li><input type="submit" class="style1" value="Send" /></li>
+														<li><input type="reset" class="style2" value="Reset" /></li>
+													</ul>
+												</div>
+											</div>
+										</form>
+									</section>
